f764b47c193c8fdca0ed57fae19394d81951d17e
David Young <davidsamuelyoung@protonmail.com>
2024-05-02 13:55:25 +0200

    Remove CMakeLists.txt; transition to Makefiles

--- cpp/src/md5.cpp
+++ cpp/src/md5.cpp
@@ -1,3 +1,10 @@
 //
 // Created by David Young on 2024/05/02.
 //
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
--- opencl/src/md5.cpp
+++ opencl/src/md5.cpp
@@ -1,8 +1,15 @@
+//
+// Created by David Young on 2024/05/02.
+//
+
+#ifdef __APPLE__
+#include <OpenCL/cl.h>
+#else
+#include<CL/cl.h>
+#endif
 #include <iostream>
 
-
-
 int main() {
-  std::cout << "Hello, World!" << std::endl;
-return 0;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
 }
